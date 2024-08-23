a44365e5db1fc483ec200df03cb30894abb8c639
root <root@Maples>
2024-08-23 07:41:20 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark': 'rgb(31,40,55)',
+        'grey': 'rgb(55,64,81)',
+        'blue': 'rgb(63,105,173)',
+        'green': 'rgb(24,162,74)',
+        'yellow': 'rgb(243,201,70)'
+      }
+    },
+  },
+  plugins: [],
+}
+

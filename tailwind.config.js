f9f6c297fe21acccf3a3febaf6b9f7d4684ea04e
root <root@Maples>
2024-08-27 10:28:30 +0000

    add content and first designs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {
-      colors: {
-        'dark': 'rgb(31,40,55)',
-        'grey': 'rgb(55,64,81)',
-        'blue': 'rgb(63,105,173)',
-        'green': 'rgb(24,162,74)',
-        'yellow': 'rgb(243,201,70)'
-      }
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            colors: {
+                dark: "rgb(31,40,55)",
+                grey: "rgb(55,64,81)",
+                blue: "rgb(39,97,216)",
+                green: "rgb(24,162,74)",
+                yellow: "rgb(243,201,70)",
+                lightred: "rgb(226,54,112)",
+                orange: "rgb(232,141,48)",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};

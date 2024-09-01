6e2b4b5dc8632b6c80bc53def0d06a60e74a4355
Philipp Runge <p.runge@progani.com>
2024-09-02 01:25:23 +0200

    feat: set up react boilerplate

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./style.css";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  console.info("Root element not found");
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}

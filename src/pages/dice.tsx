ac3e14fe601becbf17a4546b5e94ef26a477334c
root <root@Maples>
2024-09-16 14:11:49 +0000

    add states and rollDice function

--- src/pages/dice.tsx
+++ src/pages/dice.tsx
@@ -1,11 +1,27 @@
 import Button from "../components/button";
+import { useState } from "react";
 
 export default function Dice() {
+    const [maxValue, setMaxValue] = useState(20);
+    const [diceRoll, setDiceRoll] = useState<number>();
+
+    const rollDice = () => {
+        const randomNumber = Math.floor(Math.random() * maxValue) + 1;
+        setDiceRoll(randomNumber);
+    };
     return (
         <div className="h-[300px] w-[500px] border p-12 rounded-xl shadow bg-white gap-4 flex flex-col">
             <p className="font-semibold text-xl">Max Dice Value</p>
-            <input className="border rounded h-12 px-4"></input>
-            <Button onClick={() => {}}>Roll</Button>
+            <input className="border rounded h-12 px-4" value={maxValue}>
+                {setMaxValue}
+            </input>
+            <Button
+                onClick={() => {
+                    rollDice();
+                }}
+            >
+                Roll
+            </Button>
             <div className="font-semibold text-xl">Result:</div>
         </div>
     );
